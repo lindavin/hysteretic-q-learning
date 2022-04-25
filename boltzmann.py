8b0b9e7e988a2aa07cacb103a3f5ff98ac138714
diomedeg <94569088+diomedeg@users.noreply.github.com>
2022-04-25 15:14:18 -0500

    Changes

--- boltzmann.py
+++ boltzmann.py
@@ -6,27 +6,26 @@ from decimal import Decimal
 # Q is list of the estimated value of choosing each action
 # T is the system temperature
 def boltzmann_distribution(Q, T):
-    # Populate array of action selection probabilities    
-    #P = []
-    #for i in range(len(Q)):
-    #    P[i] = e**(Q[i]/(k*T))
-    
-    # DOn't know python does this work
-    P = Q
-    # Decimal to avoid overflow errors
-    # P[:] = [(Decimal(e)**(Decimal(x)/(Decimal(k)*Decimal(T)))) for x in P]
-    # Remove k constant to again avoid overflow errors
-    P[:] = [(Decimal(e)**(Decimal(x)/(Decimal(T)))) for x in P]
-    # print("P1: {}".format(P))
+    highInf = 0
+    highInfIndex = -1
+    # Check if any/multiple expected values would result in infinity, remember the highest
+    for i,x in enumerate(Q):
+        if e**(x/T) == np.Infinity and x > highInf:
+            highInf = x
+            highInfIndex = i
+
+    if highInf != 0 and highInfIndex != -1:
+        P[:] = 0
+        P[highInfIndex] = 1
+        return P
+
+    P = [e**(x/T) for x in Q]
     # Calculate canonical partition function, divide all probabilities by it so sum <= 1
     CPF = sum(P)
-    # print("CPF: {}".format(CPF))
-    P[:] = [Decimal(x) / Decimal(CPF) for x in P]
-    # print("P2: {}".format(P))
+    P[:] = [x / CPF for x in P]
 
     return P                    # Return array of probabilities
 
 # A is list of actions, Q is estimated value of each action, T is system temperature
 def selection(A, Q, T):
-    # print("calling boltzmann")
     return np.random.choice(A, p=boltzmann_distribution(Q, T))
